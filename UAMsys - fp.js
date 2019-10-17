10bf4091ba331afbd7ce9678b344aab1536980f0
Djn94-Work <55993512+Djn94-Work@users.noreply.github.com>
2019-10-17 13:56:33 -0500

    Update UAMsys - fp.js

--- UAMsys - fp.js
+++ UAMsys - fp.js
@@ -1,15 +1,32 @@
-const user = {
+userList = new Map();
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+function ValidateEmail(inputText) {
+  if (inputText.match(mailformat)) {
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    return false;
+  }
 }
-  signUp(user) {
-    console.log(user+'sign up');
-    };
-    signIn(user) {
-        console.log(user+'sign in')
-    };
-    signOut(user) {
-        console.log(user+'sign out')
-    };
-    passChange(user) {
-        console.log(user + 'password change')
-    }
+User = {
+  email = this.email,
+  password = this.password,
+  passwordConfirm = this.passwordConfirm
+}
+signUp(user){
+  if (ValidateEmail(sampleEmail){
+      
+    })
+}
+signIn(user){
+  console.log(user)
 
+}
+signOut(user){
+
+}
+passwordChange(user){
+
+}
+
+module.exports = { User };
