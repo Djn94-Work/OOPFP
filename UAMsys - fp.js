f285f46d1ea00dd04ad68d0319b2705230538130
Djn94-Work <55993512+Djn94-Work@users.noreply.github.com>
2019-10-17 13:26:20 -0500

    Update UAMsys - fp.js

--- UAMsys - fp.js
+++ UAMsys - fp.js
@@ -1,20 +1,15 @@
-// const user = {
-
-//   signUp(user) {
-//     console.log(user+'sign up');
-//     };
-//     signIn(user) {
-//         console.log(user+'sign in')
-//     };
-//     signOut(user) {
-//         console.log(user+'sign out')
-//     };
-//     passChange(user) {
-//         console.log(user + 'password change')
-//     }
-// };
-function User(email, password, passwordConfirm) {
-  this.email = email;
-  this.password = password;
-  this.passwordConfirm;
+const user = {
 }
+  signUp(user) {
+    console.log(user+'sign up');
+    };
+    signIn(user) {
+        console.log(user+'sign in')
+    };
+    signOut(user) {
+        console.log(user+'sign out')
+    };
+    passChange(user) {
+        console.log(user + 'password change')
+    }
+
